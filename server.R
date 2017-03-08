2d0caf3c0b2782646fbe0acd425ede08d0571c46
Stephen Hung <hung.stephen.yh@gmail.com>
2017-03-08 03:39:17 -0800

    Added filters on sidebar, removed errors in server1.R, added back server.R

--- server.R
+++ server.R
@@ -8,20 +8,13 @@ library(sp)
 library(leaflet)
 library(htmltools)
 
-url <- 'https://api.mapbox.com/styles/v1/connergillette/cizyy5a9m004m2smjhu438php/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29ubmVyZ2lsbGV0dGUiLCJhIjoiY2l6cWIwOWJuMDE3azMzcDdvdmx3eWMwMyJ9.Qa235PPv1uPZtvaWIUSpQA'
-
-df <- as.data.frame(read.csv('MERGED2014_15_PP.csv', stringsAsFactors = FALSE)) %>% select(INSTNM, CITY, STABBR, ZIP, LATITUDE, LONGITUDE, ADM_RATE, SATVRMID, SATMTMID, SATWRMID, starts_with('PCIP'), UGDS, TUITIONFEE_IN, TUITIONFEE_OUT
-)
-
 shinyServer(function(input, output) {
-
     url <- 'https://api.mapbox.com/styles/v1/connergillette/cizyy5a9m004m2smjhu438php/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29ubmVyZ2lsbGV0dGUiLCJhIjoiY2l6cWIwOWJuMDE3azMzcDdvdmx3eWMwMyJ9.Qa235PPv1uPZtvaWIUSpQA'
     
     df <- as.data.frame(read.csv('MERGED2014_15_PP.csv', stringsAsFactors = FALSE)) %>% select(INSTNM, CITY, STABBR, ZIP, LATITUDE, LONGITUDE, INSTURL, ADM_RATE, SATVRMID, SATMTMID, SATWRMID, starts_with('PCIP'), UGDS, TUITIONFEE_IN, TUITIONFEE_OUT
     )
     #df <- head(df, 100)
     #View(df)
-    
     
     output$map <- renderLeaflet({
         df$LATITUDE <- as.numeric(df$LATITUDE)
@@ -32,17 +25,11 @@ shinyServer(function(input, output) {
                             paste0('<b>Admission Rate: </b>', as.numeric(df$ADM_RATE) * 100, '%'), '<b>Admission Rate:</b> Not given'))
         
         leaflet(df) %>% 
-            addCircleMarkers(popup = ~htmlEscape(paste0(INSTNM, ', ', CITY)), stroke = FALSE, fillOpacity = 1, radius = 4) %>% 
-            addTiles(urlTemplate = url, attribution = 'Maps provided by <a href="http://www.mapbox.com/">Mapbox</a>') %>% 
-            setView(lng = -104.82, lat = 47.55, zoom = 3)
-        })
-
-    output$colleges <- render({
-      
             addCircleMarkers(popup = popup.content, stroke = FALSE, fillOpacity = 1, radius = 4) %>%
                         addTiles(urlTemplate = url, attribution = 'Maps provided by <a href="http://www.mapbox.com/">Mapbox</a>') %>% 
             setView(lng = -93.85, lat = 37.45, zoom = 4)
         })
-})
-
+    
+    output$list <- renderPrint({})
+    })
 
--- ui.R
+++ ui.R
@@ -1,27 +1,65 @@
 library(shiny)
 library(leaflet)
+library(dplyr)
+
+data.set <- read.csv('MERGED2014_15_PP.csv', stringsAsFactors=FALSE)
+state.abbr.list <- as.vector(unique(select(data.set,STABBR))[,1])
+state.name.list <- c()
+commonwealth.territories <- list("AS" = "American Samoa",
+                                 "DC" = "District of Columbia",
+                                 "FM" = "Federated States of Micronesia",
+                                 "GU" = "Guam",
+                                 "MH" = "Marshall Islands",
+                                 "MP" = "Northern Mariana Islands",
+                                 "PW" = "Palau",
+                                 "PR" = "Puerto Rico",
+                                 "VI" = "Virgin Islands")
+for(abbr in state.abbr.list){
+  if(abbr %in% names(commonwealth.territories)){
+    names <- commonwealth.territories[abbr]
+  }else{
+    names <- ( state.name[grep(abbr,state.abb)] )
+  }
+  state.name.list <- append(state.name.list,names)
+}
+state.name.list <- unlist(state.name.list, use.names=FALSE)
+major.list <- c("Computer Science", "Informatics")
 
 shinyUI(fluidPage(
-    titlePanel('College Finder'),
-    p('Descriptive sentence or something'),
-    
-    sidebarLayout(
-        sidebarPanel(
-            h2('Sidebar')
-            ), 
-        mainPanel(
-            #@Thejas - None of this worked for me
-            #tabPanel(type = "tabs", 
-                    # tabPanel('map', h2('Map'), leafletOutput('map')),
-                     # tabPanel('list', fluidRow(h2('List of Your Colleges'), 
-                      #                          dataTableOutput("table")),
-                      #                  fluidRow(h2('Summary statistics'), 
-                      #                           verbatimTextOutput("summary"))
-                      #),
-                     #tabPanel('all', h2('List of All Colleges'))
-            #)
-            leafletOutput('map')
-        )
+  titlePanel('College Finder'),
+  p('Descriptive sentence or something'),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("state", label=h3("State"),
+                  choices=list('States'= state.name.list)),
+      sliderInput("admission",label=h3("Admission Rate"),
+                  min=0,max=100,value=c(40,60)),
+      selectInput("major", label=h3("Major"),
+                  choices=list('Major'=major.list)),
+      sliderInput("sat",label=h3("SAT Scores"),
+                  min=0,max=800,value=c(400,700)),
+      sliderInput("size",label=h3("Undergraduate Population Size"),
+                  min=0,max=100000,value=c(30000,50000)),
+      sliderInput("tuition",label=h3("Tuition"),
+                  min=0,max=30000,value=10000)
+    ), 
+    mainPanel(
+      tabsetPanel(type = "tabs", 
+                  tabPanel('map', h2('Map'), leafletOutput('map')),
+                  tabPanel('list',  h2('List of Your Colleges'), 
+                           fluidRow(
+                             selectInput('colleges', label = "Your Colleges:", 
+                                         choices = c("No colleges"=""), 
+                                         multiple=TRUE)
+                           ),
+                           fluidRow(dataTableOutput("table")),
+                           fluidRow(h2('Summary statistics'), 
+                                    verbatimTextOutput("summary"))
+                  ),
+                  tabPanel('all', h2('List of All Colleges'))
+      )
     )
   )
+)
 )
