751918bc7446c33e4dde97327819b361fe3f3fe1
Conner Gillette <cg1100@uw.edu>
2017-03-07 23:30:08 -0800

    Add map popup functionality

--- ui.R
+++ ui.R
@@ -10,15 +10,16 @@ shinyUI(fluidPage(
             h2('Sidebar')
             ), 
         mainPanel(
-            tabpanel(type = "tabs", 
-                     tabpanel('map', h2('Map'), leafletOutput('map')),
-                      tabpanel('list', fluidRow(h2('List of Your Colleges'), 
-                                                dataTableOutput("table")),
-                                        fluidRow(h2('Summary statistics'), 
-                                                 VerbatimTextOutput("summary"))
-                      ),
-                     tabpanel('all', h2('List of All Colleges'))
-            )
+            #tabPanel(type = "tabs", 
+                    # tabPanel('map', h2('Map'), leafletOutput('map')),
+                     # tabPanel('list', fluidRow(h2('List of Your Colleges'), 
+                      #                          dataTableOutput("table")),
+                      #                  fluidRow(h2('Summary statistics'), 
+                      #                           verbatimTextOutput("summary"))
+                      #),
+                     #tabPanel('all', h2('List of All Colleges'))
+            #)
+            leafletOutput('map')
         )
     )
   )
