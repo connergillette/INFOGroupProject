2cf47877d27ea1c228c7f0d768e2d77ade315f80
Stephen Hung <hung.stephen.yh@gmail.com>
2017-03-09 00:59:16 -0800

    Cleaned up filter ranges and Information Tab

--- ui.R
+++ ui.R
@@ -3,7 +3,14 @@ library(leaflet)
 library(dplyr)
 library(shinythemes)
 
-data.set <- read.csv('MERGED2014_15_PP.csv', stringsAsFactors=FALSE)
+data.set <- read.csv('filtered_college_data.csv', stringsAsFactors=FALSE)
+data.set$TUITIONFEE_IN <- as.numeric(data.set$TUITIONFEE_IN)
+data.set$TUITIONFEE_OUT <- as.numeric(data.set$TUITIONFEE_OUT)
+data.set$ADM_RATE <- as.numeric(data.set$ADM_RATE)
+data.set$UGDS <- as.numeric(data.set$UGDS)
+data.set$SATMTMID <- as.numeric(data.set$SATMTMID)
+data.set$SATVRMID <- as.numeric(data.set$SATVRMID)
+data.set$SATWRMID <- as.numeric(data.set$SATWRMID)
 state.abbr.list <- as.vector(unique(select(data.set,STABBR))[,1])
 state.name.list <- c()
 commonwealth.territories <- list("AS" = "American Samoa",
@@ -52,7 +59,7 @@ navbarPage("College Data Information",
                       "Communication" = "PCIP09",
                       "Communications Technologies" = "PCIP10",
                       "Computer" = "PCIP11",
-                      "PersonalCulinary" = "PCIP12",
+                      "Personal & Culinary" = "PCIP12",
                       "Education" = "PCIP13",
                       "Engineering" = "PCIP14",
                       "Engineering Technology" = "PCIP15",
@@ -90,14 +97,14 @@ navbarPage("College Data Information",
         sliderInput("sat.reading",label=h3("SAT Reading Scores"),
                     min=0,max=800,value=c(0,800)),
         sliderInput("size",label=h3("Undergraduate Population Size"),
-                    min=0,max=100000,value=c(0,50000)),
+                    min=0,max=max(data.set$UGDS, na.rm=TRUE),value=c(0,30000)),
         radioButtons("type_tuition",label=h3("Type of Tuition"),
                      choices=list("No Filter" = 1, "In-State"=2, "Out-of-State"=3),
                      selected=1),
         sliderInput("in_tuition",label=h3("Tuition In-State"),
-                    min=0,max=40000,value=c(0,40000)),
+                    min=0,max=max(data.set$TUITIONFEE_IN, na.rm=TRUE),value=c(0,10000)),
         sliderInput("out_tuition",label=h3("Tuition Out-of-State"),
-                    min=0,max=40000,value=c(0,40000))
+                    min=0,max=max(data.set$TUITIONFEE_OUT,na.rm=TRUE),value=c(0,10000))
       ), 
       mainPanel(
         tabsetPanel(type = "tabs", 
@@ -141,49 +148,48 @@ navbarPage("College Data Information",
   ),
  tabPanel("Information",
     titlePanel("Information about College Finding Application"),
-    p("Education is a cornerstone to the modern society. One of
-the steps involved in the process of education is college. Nowadays,
-a degree in a college is almost a necessity for many jobs. To succeed 
-in future career, students need to pick the right college that matches 
-their preferences to spend about 4 years of their life in. Hence, it is
-important to look at the nationally accumulated data on Colleges to 
-ultimately help students pick their preferred colleges. We will be 
-observing the data on such colleges and present it in a helpful way 
-for the users to understand. The source that we will be using comes 
-from the source Data.gov, the link to which has been provided below.
-The data is collected, managed and hosted by the U.S. General Services
-Administration, Technology Transformation Service. We aim to make use
-of specific subsets of the data sets such as most recent cohorts 
-scorecard, and college scorecard raw data. These data sets will help 
-us relate the different requirements of different schools throughout 
-the nation and also provide us with information like top 75 percentile
-admit SAT score, acceptance rate, city of the respective university, 
-amount of earnings by graduates etc. Along with data sets we will also
-be using a data dictionary, the link to that is also provided below.
-"),
-
+        h3('Thesis'),
+      p("Education is a cornerstone to the modern society. One of
+      the steps involved in the process of education is college. Nowadays,
+      a degree in a college is almost a necessity for many jobs. To succeed 
+      in future career, students need to pick the right college that matches 
+      their preferences to spend about 4 years of their life in. Hence, it is
+      important to look at the nationally accumulated data on Colleges to 
+      ultimately help students pick their preferred colleges.",br(),br(),"We will be 
+      observing the data on such colleges and present it in a helpful way 
+      for the users to understand. The source that we will be using comes 
+      from the source Data.gov, the link to which has been provided below.
+      The data is collected, managed and hosted by the U.S. General Services
+      Administration, Technology Transformation Service.",br(),br(),"We aim to make use
+      of specific subsets of the 2014-2015 data set. This data set will help 
+      us relate the different requirements of different schools throughout 
+      the nation and also provide us with information like top 75 percentile
+      admit SAT score, acceptance rate, city of the respective university, 
+      tuition fees, etc.
+      "),
+         h3('Target Audience'),
       p("The major audience that we hope to aim are the graduating high
-school seniors who will be looking at different colleges to apply to. 
-There dire need to compare the 75/25 percentile of each college to
-their own scores, tuition fee of each university and suitable city will
-be fulfilled by this data representation. For example, John Wick, an 
-average american high school senior who has bagged a decent score in 
-SAT, limited financial standing and now wants to apply in different 
-colleges. Since he cannot apply to all of the colleges (due to high 
-application fee), John has to finalize few colleges where he thinks 
-he can get in and where his other requirements are met. He turns to 
-the information provided by us and he can easily list down universities
-according to his needs. Another audience that we can target will be 
-the high school counselors who need to keep a track of each university,
-their requirements, tuition fee etc., so that they can be helpful 
-towards their students seeking guidance. For example, Josh Spencer, 
-a counselor at a public school, would need to consider the academic,
-financial and vocational situations of each of the student to suggest
-appropriate schools. Our presentation can assist people like Josh to
-consider preferable options for their students based on our easily 
-understandable data and their experience.
-"),
-    p("Reference: https://collegescorecard.ed.gov/data/documentation/ 
-      By U.S. Department of Education")
+      school seniors who will be looking at different colleges to apply to. 
+      There dire need to compare the 75/25 percentile of each college to
+      their own scores, tuition fee of each university and suitable city will
+      be fulfilled by this data representation.",br(),br(),"For example, John Wick, an 
+      average american high school senior who has bagged a decent score in 
+      SAT, limited financial standing and now wants to apply in different 
+      colleges. Since he cannot apply to all of the colleges (due to high 
+      application fee), John has to finalize few colleges where he thinks 
+      he can get in and where his other requirements are met. He turns to 
+      the information provided by us and he can easily list down universities
+      according to his needs.",br(),br(),"Another audience that we can target will be 
+      the high school counselors who need to keep a track of each university,
+      their requirements, tuition fee etc., so that they can be helpful 
+      towards their students seeking guidance.",br(),br(),"For example, Josh Spencer, 
+      a counselor at a public school, would need to consider the academic,
+      financial and vocational situations of each of the student to suggest
+      appropriate schools. Our presentation can assist people like Josh to
+      consider preferable options for their students based on our easily 
+      understandable data and their experience.
+      "),
+          h3('Reference'),
+      p("US Department of Education: https://collegescorecard.ed.gov/data/documentation/")
     )
 )))
