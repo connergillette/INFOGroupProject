9a464ec4cf4dff27035eb175fbd6815b55f4dc7d
Thejas Vidyasagar <pvthejas@gmail.com>
2017-03-08 18:53:27 -0800

    finished list tab

--- ui.R
+++ ui.R
@@ -1,65 +1,43 @@
 library(shiny)
 library(leaflet)
-library(dplyr)
 
-data.set <- read.csv('MERGED2014_15_PP.csv', stringsAsFactors=FALSE)
-state.abbr.list <- as.vector(unique(select(data.set,STABBR))[,1])
-state.name.list <- c()
-commonwealth.territories <- list("AS" = "American Samoa",
-                                 "DC" = "District of Columbia",
-                                 "FM" = "Federated States of Micronesia",
-                                 "GU" = "Guam",
-                                 "MH" = "Marshall Islands",
-                                 "MP" = "Northern Mariana Islands",
-                                 "PW" = "Palau",
-                                 "PR" = "Puerto Rico",
-                                 "VI" = "Virgin Islands")
-for(abbr in state.abbr.list){
-  if(abbr %in% names(commonwealth.territories)){
-    names <- commonwealth.territories[abbr]
-  }else{
-    names <- ( state.name[grep(abbr,state.abb)] )
-  }
-  state.name.list <- append(state.name.list,names)
-}
-state.name.list <- unlist(state.name.list, use.names=FALSE)
-major.list <- c("Computer Science", "Informatics")
+df <- as.data.frame(read.csv('MERGED2014_15_PP.csv', stringsAsFactors = FALSE)) %>% select(INSTNM, CITY, STABBR, ZIP, LATITUDE, LONGITUDE, INSTURL, ADM_RATE, SATVRMID, SATMTMID, SATWRMID, starts_with('PCIP'), UGDS, TUITIONFEE_IN, TUITIONFEE_OUT)
+df <- filter(df, !is.na(LATITUDE)) %>% filter(!is.na(LONGITUDE))
+
+
+is.data.frame(df)
 
 shinyUI(fluidPage(
-  titlePanel('College Finder'),
-  p('Descriptive sentence or something'),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("state", label=h3("State"),
-                  choices=list('States'= state.name.list)),
-      sliderInput("admission",label=h3("Admission Rate"),
-                  min=0,max=100,value=c(40,60)),
-      selectInput("major", label=h3("Major"),
-                  choices=list('Major'=major.list)),
-      sliderInput("sat",label=h3("SAT Scores"),
-                  min=0,max=800,value=c(400,700)),
-      sliderInput("size",label=h3("Undergraduate Population Size"),
-                  min=0,max=100000,value=c(30000,50000)),
-      sliderInput("tuition",label=h3("Tuition"),
-                  min=0,max=30000,value=10000)
-    ), 
-    mainPanel(
-      tabsetPanel(type = "tabs", 
-                  tabPanel('map', h2('Map'), leafletOutput('map')),
-                  tabPanel('list',  h2('List of Your Colleges'), 
-                           fluidRow(
-                             selectInput('colleges', label = "Your Colleges:", 
-                                         choices = c("No colleges"=""), 
-                                         multiple=TRUE)
-                           ),
-                           fluidRow(dataTableOutput("table")),
-                           fluidRow(h2('Summary statistics'), 
-                                    verbatimTextOutput("summary"))
-                  ),
-                  tabPanel('all', h2('List of All Colleges'))
-      )
+    titlePanel('College Finder'),
+    p('Descriptive sentence or something'),
+    
+    sidebarLayout(
+        sidebarPanel(
+            h2('Sidebar')
+            ), 
+        mainPanel(
+            tabsetPanel(type = "tabs", 
+                     tabPanel('map', h2('Map'), leafletOutput('map')),
+                      tabPanel('list',  h2('List of Your Colleges'), 
+                               fluidRow(
+                                 selectInput('colleges1', label = "College 1:", 
+                                             choices = c(df$INSTNM)), 
+                                 selectInput('colleges2', label = "College 2:", 
+                                             choices = c(df$INSTNM)), 
+                                 selectInput('colleges3', label = "College 3:", 
+                                             choices = c(df$INSTNM)),
+                                 selectInput('colleges4', label = "College 4:", 
+                                             choices = c(df$INSTNM)), 
+                                 selectInput('colleges5', label = "College 5:", 
+                                             choices = c(df$INSTNM))
+                                 ),
+                               fluidRow(dataTableOutput("table")),
+                               fluidRow(h2('Summary statistics'), 
+                                        verbatimTextOutput("summary"))
+                      ),
+                     tabPanel('all', h2('List of All Colleges'))
+            )
+        )
     )
   )
-)
 )
